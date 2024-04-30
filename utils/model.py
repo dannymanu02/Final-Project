1a11a6cf6e0c9345f1b68ad2716d8b605c298383
Dannana Manoj <46117765+dannymanu02@users.noreply.github.com>
2024-04-30 15:33:58 -0400

    comm

--- utils/model.py
+++ utils/model.py
@@ -31,20 +31,20 @@ class model(object):
             print(e)
     def token_generator(df_titles):
         try:
-            print("Before Tokens")
+
             tokenizer = Tokenizer()
             df_titles['Cleaned_Title'] = df_titles['Cleaned_Title'].astype(str)
             df_titles['Cleaned_Title'] = df_titles['Cleaned_Title'].str.lower().replace('[^\w\s]', '', regex=True)
             tokenizer.fit_on_texts(df_titles['Cleaned_Title'])
             sequences = tokenizer.texts_to_sequences(df_titles['Cleaned_Title'])
             maxlen = max(len(seq) for seq in sequences)
-            print("After Tokens")
+
             return tokenizer, maxlen
         except Exception as e:
             print(e)
     
     def load_model_ltsm():
         try: 
-            return tf.keras.models.load_model('LTSM.keras') 
+            return tf.keras.models.load_model('LTSM.keras', compile=False) 
         except Exception as e:
             print(e)
